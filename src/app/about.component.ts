d25771d705bc108b3f3922436500d98dc67528da
Nattasak <dottedes@gmail.com>
2017-11-19 21:40:20 +0700

    add image-placeholder show before real-image loaded

--- src/app/about.component.ts
+++ src/app/about.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   template: `
-  <h3 class="title">About</h3>
-  <div class="card">
-    <div class="card-body">
-      About description
+    <h3 class="title">About</h3>
+    <div class="card">
+      <div class="card-body">
+        About description
+      </div>
     </div>
-  </div>
-`
+  `
 })
 export class AboutComponent { }

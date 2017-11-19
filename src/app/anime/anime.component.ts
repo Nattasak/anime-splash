5b1ce957948a9725a9f27ef2e0b6f1994a4abcf0
Nattasak <dottedes@gmail.com>
2017-11-19 20:22:31 +0700

    update anime-page content and component

--- src/app/anime/anime.component.ts
+++ src/app/anime/anime.component.ts
@@ -1,15 +1,33 @@
+// Angular Module
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse  } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-anime',
   templateUrl: './anime.component.html',
   styleUrls: ['./anime.component.css']
 })
+
+// Class
 export class AnimeComponent implements OnInit {
+  results: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
   }
 
+  ngOnInit(): void {
+    this.http.get('https://ghibliapi.herokuapp.com/films').subscribe(
+      data => {
+        this.results = data;
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-side error occured.");
+        } else {
+          console.log("Server-side error occured.");
+        }
+      }
+    );
+  }
 }
